cdbbdb39effaed060d649052225d88f63d9f649e
SebastianVillar <seba@sebastillar.uy>
2022-07-16 11:14:45 -0300

    Add config and initial packages and dependencies

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,27 +14,24 @@ use App\Http\Controllers\CheckoutController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('cart')->group(function () {
+Route::prefix("cart")->group(function () {
     #HEALTHCHECK
-    Route::get('/healthcheck', function () {
-        return 'OK';
+    Route::get("/healthcheck", static function () {
+        return "OK";
     });
 
     #CHECKOUT
-    Route::post('/checkout/{orderId}', [CheckoutController::class, 'store']);    
+    Route::post("/checkout/{orderId}", [CheckoutController::class, "store"]);
 
     #ORDERS
-    Route::prefix('orders')->group(function(){
-        Route::delete('/{orderId}/{productId}', [OrderController::class, 'delete']);        
-        Route::get('/{orderId}', [OrderController::class, 'show']);      
-        Route::post('/store', [OrderController::class, 'store']);
-        Route::put('/{orderId}', [OrderController::class, 'update']); 
-    });     
-
+    Route::prefix("orders")->group(function () {
+        Route::delete("/{orderId}/{productId}", [OrderController::class, "delete"]);
+        Route::get("/{orderId}", [OrderController::class, "show"]);
+        Route::post("/store", [OrderController::class, "store"]);
+        Route::put("/{orderId}", [OrderController::class, "update"]);
+    });
 });
-
-
