ccdb4c2212f3354e9f273eaae1436c9975584162
SebastianVillar <seba@sebastillar.uy>
2022-07-17 19:25:24 -0300

    Add migrations

---
+++ database/migrations/2022_07_17_205053_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("customers", function (Blueprint $table) {
+            $table->id();
+            $table->json("shipment_address")->nullable();
+            $table->json("billing_address")->nullable();
+            $table->json("payment_method")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("customers");
+    }
+}
